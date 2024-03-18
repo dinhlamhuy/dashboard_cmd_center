efaa88d41491974983bcc2ab7b3fd0bef937bd27
dinhlamhuy <dinhlamhuytak489@gmail.com>
2024-03-18 21:31:48 +0700

    ip

---
+++ src/component/ModalScreens/index.js
@@ -0,0 +1,46 @@
+import React, { ReactNode, useEffect } from 'react'
+import Modal from 'react-modal'
+import './index.css';
+// type ModalProps = {
+//   isOpen: boolean
+//   onClose: () => void
+//   children: ReactNode
+// }
+
+const ModalScreens = ({ isOpen, onClose, children }) => {
+  useEffect(() => {
+    Modal.setAppElement('#root');
+  }, []);
+  return (
+    <Modal 
+      isOpen={isOpen}
+      onRequestClose={onClose}
+      contentLabel='Modal'
+      style={{
+        overlay: {
+          backgroundColor: 'rgba(0, 0, 0, 0.5)'
+        },
+        content: {
+          width: '50%',
+          minWidth: '40%',
+          maxWidth: '80%',
+          maxHeight: '60%',
+          minHeight: '20%',
+          margin: 'auto',
+          // display: 'flex',
+          flexDirection: 'column',
+          // justifyContent: 'center',
+          // alignItems: 'center',
+          border: '10px solid teal',
+          borderRadius: '12px',
+           overflow:"hidden",
+          boxShadow: '0 2px 4px rgba(0, 0, 0, 0.8)',
+          padding: '0px',
+          backgroundColor: '#000000'
+        }
+      }}
+    >{children}</Modal>
+  )
+}
+
+export default ModalScreens
--- src/screens/home/HomeScreen.js
+++ src/screens/home/HomeScreen.js
@@ -3,6 +3,7 @@ import React, { useEffect, useRef, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import socketIOClient from "socket.io-client";
 import MenuBar from "../../component/MenuBar";
+import ModalScreens from "../../component/ModalScreens";
 const HomeScreen = () => {
   const navigate = useNavigate();
   const [socket, setSocket] = useState(null);
@@ -14,7 +15,8 @@ const HomeScreen = () => {
   const dragOverItem = useRef();
   const [screenFrom, setScreenFrom] = useState(-1);
   const [screenTo, setScreenTo] = useState(-1);
-
+  const [openModal, setOpenModal] = useState(false);
+  const [itemScreen, setItemScreen] = useState({});
   const data = [
     { id_screen: '1', label: 'DDM', descriptionL: '' },
     { id_screen: '2', label: 'STOPLINE', descriptionL: '' },
@@ -35,32 +37,36 @@ const HomeScreen = () => {
   ]
 
   const [list, setList] = useState(data)
-  useEffect(() => {
+  // useEffect(() => {
+  //   const getAPI = async () => {
+  //     await axios.post('http://localhost:8083/dashboard/getlistscreen', { list: list }).then(response => {
+  //       console.log(response.data.data)
+  //       setList(response.data.data.data)
+  //     }).catch(() => {
+  //     })
+  //   }
+  //   getAPI()
+  //   socketRef.current = socketIOClient.connect('http://192.168.1.13:8083');
+  //   socketRef.current.on("message", (data) => {
+  //     console.log(data);
+  //   });
+  //   socketRef.current.on(`54314`, (data) => {
+  //     console.log('data', data)
+  //     setSocket(data);
+  //   });
+  //   return () => {
+  //     socketRef.current.disconnect();
+  //   };
+  // }, [socket])
+  const openModalScreen = (id, item) => {
+   
+    setItemScreen(item);
+    setOpenModal(true);
+  }
+  const closeModalScreen = () => {
+    setOpenModal(false);
+  }
 
-    const getAPI = async () => {
-      await axios.post('http://localhost:8083/dashboard/getlistscreen', { list: list }).then(response => {
-        // console.log(response.data.data)
-        setList(response.data.data.data)
-      }).catch(() => {
-
-      })
-    }
-
-
-    // getAPI()
-
-    socketRef.current = socketIOClient.connect('http://192.168.1.13:8083');
-    socketRef.current.on("message", (data) => {
-      console.log(data);
-    });
-    socketRef.current.on(`54314`, (data) => {
-      console.log('data', data)
-      setSocket(data);
-    });
-    return () => {
-      socketRef.current.disconnect();
-    };
-  }, [socket])
 
 
   const swapdrop = (idscreenTo) => {
@@ -70,16 +76,22 @@ const HomeScreen = () => {
     copyListItems[idscreenTo] = temp;
     setScreenFrom(-1)
     setScreenTo(-1)
-    // getAPI(copyListItems);
 
     setList(copyListItems);
   };
-  function ChooseScreen(index) {
-
+  const ChooseScreen = (index, item) => {
+    // console.log(item);
     if (screenFrom === -1) {
-      setScreenFrom(index)
-      setScreenTo(-1);
+      if (item.label === '') {
+        openModalScreen(index, item)
+        return;
+      } else {
+        setScreenFrom(index)
+        setScreenTo(-1);
+      }
     } else if (index === screenFrom) {
+      openModalScreen(index, item)
+
       setScreenFrom(-1);
       setScreenTo(-1);
     } else {
@@ -94,12 +106,10 @@ const HomeScreen = () => {
     <>
       <MenuBar>
 
-
-      
         <div className="w-full h-screen bg-gray-900 grid p-0 m-0 grid-cols-4 gap-3 ">
           {list.map((item, index) => {
             return (
-              <button onClick={() => ChooseScreen(index)} className={` h-full w-full   font-bold rounded-lg`} key={'frame' + index}>
+              <button onClick={() => ChooseScreen(index, item)} className={` h-full w-full   font-bold rounded-lg`} key={'frame' + index}>
                 <div
                   className={`${screenFrom == index ? 'ring-4 ring-fuchsia-950 shadow-2xl-ring-offset-4 bg-red-800 text-white' : ' backdrop-blur-md bg-gray-950 text-yellow-400 '} 
               text-xl select-none text-ellipsis overflow-hidden rounded-lg border-dashed  border-2 border-orange-400 drag w-full h-full flex justify-center items-center p-0 m-0 text-black`}>
@@ -113,6 +123,19 @@ const HomeScreen = () => {
 
         </div>
       </MenuBar>
+      <ModalScreens isOpen={openModal}>
+        <div className="flex justify-end p-3">
+          <button className="text-2xl rounded-full bg-gray-200 px-2.5 " onClick={closeModalScreen}>X</button>
+        </div>
+        <div className="flex justify-center items-center text-white text-3xl font-bold">Màn hình số {itemScreen.id_screen}</div>
+        <div >
+          
+        </div>
+          <div className=" absolute bottom-0 left-1/2 transform -translate-x-1/2 flex justify-center items-center p-3 ">
+            <button className="bg-blue-600 mx-2 px-4 py-2 rounded-xl text-lg font-bold text-white">Chọn</button>
+            <button onClick={closeModalScreen} className="bg-gray-400 mx-2 px-4 py-2 rounded-xl text-lg font-bold text-white">Hủy</button>
+          </div>
+      </ModalScreens>
     </>
   );
 };
