429e509a1d3f2d63e5e18be9ed9150821624f449
dinhlamhuy <dinhlamhuytak489@gmail.com>
2024-03-25 18:47:55 +0700

    upload4

--- src/screens/home/HomeScreen.js
+++ src/screens/home/HomeScreen.js
@@ -28,118 +28,118 @@ const HomeScreen = () => {
 
   const [list, setList] = useState([
     {
-      DB_id: "10",
-      DB_url: null,
-      DB_name: "HOURLY OUTPUT BY MODEL",
-      Screen_id: "1",
-      Screen_position: "1",
+        "DB_id": "10",
+        "DB_url": null,
+        "DB_name": "HOURLY OUTPUT BY MODEL",
+        "Screen_id": "1",
+        "Screen_position": "1"
     },
     {
-      DB_id: "14",
-      DB_url: "StockFitting",
-      DB_name: "STOCK FITTING QUALITY",
-      Screen_id: "2",
-      Screen_position: "2",
+        "DB_id": "2",
+        "DB_url": null,
+        "DB_name": "IPARKING",
+        "Screen_id": "2",
+        "Screen_position": "2"
     },
     {
-      DB_id: "2",
-      DB_url: null,
-      DB_name: "IPARKING",
-      Screen_id: "3",
-      Screen_position: "3",
+        "DB_id": "14",
+        "DB_url": "StockFittingQuality",
+        "DB_name": "STOCK FITTING QUALITY",
+        "Screen_id": "3",
+        "Screen_position": "3"
     },
     {
-      DB_id: "16",
-      DB_url: null,
-      DB_name: "HOURLY OUTPUT BY FLOOR 2",
-      Screen_id: "4",
-      Screen_position: "4",
+        "DB_id": "16",
+        "DB_url": null,
+        "DB_name": "HOURLY OUTPUT BY FLOOR 2",
+        "Screen_id": "4",
+        "Screen_position": "4"
     },
     {
-      DB_id: "13",
-      DB_url: null,
-      DB_name: "BÁO CÁO SẢN LƯỢNG",
-      Screen_id: "5",
-      Screen_position: "5",
+        "DB_id": "13",
+        "DB_url": null,
+        "DB_name": "BÁO CÁO SẢN LƯỢNG",
+        "Screen_id": "5",
+        "Screen_position": "5"
     },
     {
-      DB_id: "5",
-      DB_url: null,
-      DB_name: "AVAILABILITY KPI",
-      Screen_id: "6",
-      Screen_position: "6",
+        "DB_id": "5",
+        "DB_url": null,
+        "DB_name": "AVAILABILITY KPI",
+        "Screen_id": "6",
+        "Screen_position": "6"
     },
     {
-      DB_id: "15",
-      DB_url: "HourlyOutPutByFloor",
-      DB_name: "HOURLY OUTPUT BY FLOOR",
-      Screen_id: "7",
-      Screen_position: "7",
+        "DB_id": "15",
+        "DB_url": "HourlyOutPutByFloor",
+        "DB_name": "HOURLY OUTPUT BY FLOOR",
+        "Screen_id": "7",
+        "Screen_position": "7"
     },
     {
-      DB_id: "7",
-      DB_url: null,
-      DB_name: "ASSEMBLY QUALITY",
-      Screen_id: "8",
-      Screen_position: "8",
+        "DB_id": "7",
+        "DB_url": "AssemblyQuality",
+        "DB_name": "ASSEMBLY QUALITY",
+        "Screen_id": "8",
+        "Screen_position": "8"
     },
     {
-      DB_id: "11",
-      DB_url: null,
-      DB_name: "HOURLY OUTPUT BY MODEL 2",
-      Screen_id: "9",
-      Screen_position: "9",
+        "DB_id": "11",
+        "DB_url": null,
+        "DB_name": "HOURLY OUTPUT BY MODEL 2",
+        "Screen_id": "9",
+        "Screen_position": "9"
     },
     {
-      DB_id: "17",
-      DB_url: "HR",
-      DB_name: "HR",
-      Screen_id: "10",
-      Screen_position: "10",
+        "DB_id": "17",
+        "DB_url": "HR",
+        "DB_name": "HR",
+        "Screen_id": "10",
+        "Screen_position": "10"
     },
     {
-      DB_id: "8",
-      DB_url: null,
-      DB_name: "STOCK FITTING ",
-      Screen_id: "11",
-      Screen_position: "11",
+        "DB_id": "8",
+        "DB_url": null,
+        "DB_name": "STOCK FITTING ",
+        "Screen_id": "11",
+        "Screen_position": "11"
     },
     {
-      DB_id: "4",
-      DB_url: "Data_KPI_Total",
-      DB_name: "QUALITY KPI TOTAL",
-      Screen_id: "12",
-      Screen_position: "12",
+        "DB_id": "4",
+        "DB_url": "Data_KPI_Total",
+        "DB_name": "QUALITY KPI TOTAL",
+        "Screen_id": "12",
+        "Screen_position": "12"
     },
     {
-      DB_id: "3",
-      DB_url: null,
-      DB_name: "CSS TRIAL SCHEDULE",
-      Screen_id: "13",
-      Screen_position: "13",
+        "DB_id": "3",
+        "DB_url": null,
+        "DB_name": "CSS TRIAL SCHEDULE",
+        "Screen_id": "13",
+        "Screen_position": "13"
     },
     {
-      DB_id: "1",
-      DB_url: null,
-      DB_name: "ISTOCKING",
-      Screen_id: "14",
-      Screen_position: "14",
+        "DB_id": "1",
+        "DB_url": null,
+        "DB_name": "ISTOCKING",
+        "Screen_id": "14",
+        "Screen_position": "14"
     },
     {
-      DB_id: "9",
-      DB_url: null,
-      DB_name: "PROD KPI DASHBOARD",
-      Screen_id: "15",
-      Screen_position: "15",
+        "DB_id": "9",
+        "DB_url": null,
+        "DB_name": "PROD KPI DASHBOARD",
+        "Screen_id": "15",
+        "Screen_position": "15"
     },
     {
-      DB_id: "6",
-      DB_url: null,
-      DB_name: "SHIPPING SCHEDULE",
-      Screen_id: "16",
-      Screen_position: "16",
-    },
-  ]);
+        "DB_id": "6",
+        "DB_url": null,
+        "DB_name": "SHIPPING SCHEDULE",
+        "Screen_id": "16",
+        "Screen_position": "16"
+    }
+]);
 
   useEffect(() => {
     const getAPIcheck = async () => {
